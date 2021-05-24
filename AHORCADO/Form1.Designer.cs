b89025dd1b15da549e2334722ccc1018b4fb5e10
pcpisquiy <pcpisquiy@gmail.com>
2021-05-24 11:57:54 -0600

    first commit

---
+++ AHORCADO/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AHORCADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtletra = new System.Windows.Forms.TextBox();
+            this.txtletra1 = new System.Windows.Forms.TextBox();
+            this.txtletra2 = new System.Windows.Forms.TextBox();
+            this.txtletra3 = new System.Windows.Forms.TextBox();
+            this.txtletra4 = new System.Windows.Forms.TextBox();
+            this.txtletra5 = new System.Windows.Forms.TextBox();
+            this.txtletra6 = new System.Windows.Forms.TextBox();
+            this.txtletra7 = new System.Windows.Forms.TextBox();
+            this.txtletra8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtletra
+            // 
+            this.txtletra.Location = new System.Drawing.Point(28, 25);
+            this.txtletra.Name = "txtletra";
+            this.txtletra.Size = new System.Drawing.Size(32, 20);
+            this.txtletra.TabIndex = 0;
+            this.txtletra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtletra_KeyPress);
+            // 
+            // txtletra1
+            // 
+            this.txtletra1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra1.ForeColor = System.Drawing.Color.White;
+            this.txtletra1.Location = new System.Drawing.Point(135, 67);
+            this.txtletra1.Name = "txtletra1";
+            this.txtletra1.ReadOnly = true;
+            this.txtletra1.Size = new System.Drawing.Size(37, 20);
+            this.txtletra1.TabIndex = 1;
+            this.txtletra1.Visible = false;
+            // 
+            // txtletra2
+            // 
+            this.txtletra2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra2.ForeColor = System.Drawing.Color.White;
+            this.txtletra2.Location = new System.Drawing.Point(199, 67);
+            this.txtletra2.Name = "txtletra2";
+            this.txtletra2.ReadOnly = true;
+            this.txtletra2.Size = new System.Drawing.Size(37, 20);
+            this.txtletra2.TabIndex = 2;
+            this.txtletra2.Visible = false;
+            // 
+            // txtletra3
+            // 
+            this.txtletra3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra3.ForeColor = System.Drawing.Color.White;
+            this.txtletra3.Location = new System.Drawing.Point(269, 67);
+            this.txtletra3.Name = "txtletra3";
+            this.txtletra3.ReadOnly = true;
+            this.txtletra3.Size = new System.Drawing.Size(37, 20);
+            this.txtletra3.TabIndex = 3;
+            this.txtletra3.Visible = false;
+            // 
+            // txtletra4
+            // 
+            this.txtletra4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra4.ForeColor = System.Drawing.Color.White;
+            this.txtletra4.Location = new System.Drawing.Point(345, 67);
+            this.txtletra4.Name = "txtletra4";
+            this.txtletra4.ReadOnly = true;
+            this.txtletra4.Size = new System.Drawing.Size(37, 20);
+            this.txtletra4.TabIndex = 4;
+            this.txtletra4.Visible = false;
+            // 
+            // txtletra5
+            // 
+            this.txtletra5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra5.ForeColor = System.Drawing.Color.White;
+            this.txtletra5.Location = new System.Drawing.Point(409, 67);
+            this.txtletra5.Name = "txtletra5";
+            this.txtletra5.ReadOnly = true;
+            this.txtletra5.Size = new System.Drawing.Size(37, 20);
+            this.txtletra5.TabIndex = 5;
+            this.txtletra5.Visible = false;
+            // 
+            // txtletra6
+            // 
+            this.txtletra6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra6.ForeColor = System.Drawing.Color.White;
+            this.txtletra6.Location = new System.Drawing.Point(474, 67);
+            this.txtletra6.Name = "txtletra6";
+            this.txtletra6.ReadOnly = true;
+            this.txtletra6.Size = new System.Drawing.Size(37, 20);
+            this.txtletra6.TabIndex = 6;
+            this.txtletra6.Visible = false;
+            // 
+            // txtletra7
+            // 
+            this.txtletra7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra7.ForeColor = System.Drawing.Color.White;
+            this.txtletra7.Location = new System.Drawing.Point(532, 67);
+            this.txtletra7.Name = "txtletra7";
+            this.txtletra7.ReadOnly = true;
+            this.txtletra7.Size = new System.Drawing.Size(37, 20);
+            this.txtletra7.TabIndex = 7;
+            this.txtletra7.Visible = false;
+            // 
+            // txtletra8
+            // 
+            this.txtletra8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtletra8.ForeColor = System.Drawing.Color.White;
+            this.txtletra8.Location = new System.Drawing.Point(595, 67);
+            this.txtletra8.Name = "txtletra8";
+            this.txtletra8.ReadOnly = true;
+            this.txtletra8.Size = new System.Drawing.Size(37, 20);
+            this.txtletra8.TabIndex = 8;
+            this.txtletra8.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtletra8);
+            this.Controls.Add(this.txtletra7);
+            this.Controls.Add(this.txtletra6);
+            this.Controls.Add(this.txtletra5);
+            this.Controls.Add(this.txtletra4);
+            this.Controls.Add(this.txtletra3);
+            this.Controls.Add(this.txtletra2);
+            this.Controls.Add(this.txtletra1);
+            this.Controls.Add(this.txtletra);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtletra;
+        private System.Windows.Forms.TextBox txtletra1;
+        private System.Windows.Forms.TextBox txtletra2;
+        private System.Windows.Forms.TextBox txtletra3;
+        private System.Windows.Forms.TextBox txtletra4;
+        private System.Windows.Forms.TextBox txtletra5;
+        private System.Windows.Forms.TextBox txtletra6;
+        private System.Windows.Forms.TextBox txtletra7;
+        private System.Windows.Forms.TextBox txtletra8;
+    }
+}
+
---
+++ AHORCADO/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace AHORCADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string palabra_aleatoria = "";
+        private void msgbox(string mensaje, MessageBoxButtons btns_stile, MessageBoxIcon icons) {
+            MessageBox.Show(mensaje,"AHORCADO",btns_stile,icons);
+
+        }
+        private void seleccionar_palabra() {
+            Random rnd = new Random();
+            int numero = rnd.Next(1,15);
+            switch (numero)
+            {
+                case 1:
+                    palabra_aleatoria = "mono";
+                    break;
+                case 2:
+                    palabra_aleatoria = "perro";
+                    break;
+                case 3:
+                    palabra_aleatoria = "pajaro";
+                    break;
+                case 4:
+                    palabra_aleatoria = "minero";
+                    break;
+                case 5:
+                    palabra_aleatoria = "macaco";
+                    break;
+                case 6:
+                    palabra_aleatoria = "mono";
+                    break;
+                case 7:
+                     palabra_aleatoria = "cocina";
+                    break;
+                case 8:
+                     palabra_aleatoria = "cama";
+                    break;
+                case  9:
+                    palabra_aleatoria = "aguja";
+                    break;
+                case 10:
+                     palabra_aleatoria = "cuaderno";
+                    break;
+                case 11:
+                    palabra_aleatoria = "espejo";
+                    break;
+                case 12:
+                    palabra_aleatoria = "piedra";
+                    break;
+               case 13:
+                     palabra_aleatoria = "tierra";
+                    break;
+                case 14:
+                    palabra_aleatoria = "bandera";
+                    break;
+                 case 15:
+                palabra_aleatoria = "baño";
+                    break;
+            }
+            MessageBox.Show(palabra_aleatoria.ToString());
+        }
+        private void limpiar_mostrar_textbox() {
+            try {
+
+                foreach (TextBox txtbox in this.Controls)
+                {
+                    if (txtbox is TextBox && txtbox != txtletra)
+                    {
+                        txtbox.Clear();
+                        txtbox.Hide();
+                    }
+                }
+                int limite = palabra_aleatoria.Length;
+                for (int i = 1; i <= limite; i++)
+                {
+                    string txt_name = "txtletra" + i.ToString();
+                    TextBox pbletra = this.Controls.Find(txt_name,true).FirstOrDefault() as TextBox;
+                    pbletra.Show();
+
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("ERROR: {0}", ex.Message);
+            }
+
+        }
+        private void colocar_letra() {
+            for (int x = 0; x < palabra_aleatoria.Length; x++) {
+
+                if (txtletra.Text.ToUpper() == palabra_aleatoria[x].ToString().ToUpper()) {
+                    string txt_name = "txtletra" + (x + 1).ToString();
+                    TextBox pbletra = this.Controls.Find(txt_name, true).FirstOrDefault() as TextBox;
+                    pbletra.Text = palabra_aleatoria[x].ToString();
+                    txtletra.Clear();
+                }
+            }
+
+        }
+        private void Iniciar_partida() {
+
+            msgbox("BIENVENIDO, DEBE INGRESAR LAS LETRAS",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            palabra_aleatoria = "";
+            seleccionar_palabra();
+            limpiar_mostrar_textbox();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Iniciar_partida();
+        }
+
+        private void txtletra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (this.txtletra.Text.Length > 1 )
+            {
+                txtletra.Clear();
+                MessageBox.Show("El dato ingresado es invalido, ingrese una sola letra");
+                txtletra.Clear();
+            }
+            else {
+                colocar_letra();
+            }
+        }
+    }
+}
